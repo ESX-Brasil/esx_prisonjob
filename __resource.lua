09fe684ffe54d34a8f798196b525ee521a090296
renildomarcio <krsolucoesweb@gmail.com>
2019-05-10 05:40:48 -0300

    Update #1.1.0
    - Add Tradução em Portugues Brasil!
    - Fix Bugs

--- __resource.lua
+++ __resource.lua
@@ -2,11 +2,12 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Prison Job'
 
-version '1.0.0'
+version '1.1.0'
 
 client_scripts {
     '@es_extended/locale.lua',
     'locales/en.lua',
+    'locales/br.lua',
     'jobs/electrician.lua'
     'config.lua'
 }
@@ -14,6 +15,7 @@ client_scripts {
 server_scripts {
     '@es_extended/locale.lua',
     'locales/en.lua',
+    'locales/br.lua',
     'config.lua',
     'server/esx_jobs_sv.lua'
 }
---
+++ locales/br.lua
@@ -0,0 +1,15 @@
+Locales['br'] = {
+    ['jailtime_reduced']          = 'Seu tempo de prisão foi resgatado em ~g~segundos pelo seu trabalho.',
+    ['max_limit']                 = 'Seu estoque está com capacidade total!',
+    ['not_enough']                = 'você não tem o suficiente',
+    ['not_enough2']               = 'no seu inventário!',
+
+    -- Electrician job
+    ['lj_mapblip']                = 'gerador quebrado',
+    ['lj_broken_parts']           = 'peças quebradas',
+    ['lj_pickup']                 = 'pressione ~INPUT_PICKUP~ para recuperar peças quebradas',
+    ['lj_delivery_point']         = 'ponto de entrega de partes quebrado',
+    ['lj_delivery']               = 'entrega partes quebrado',
+    ['lj_broken_scraps']          = 'sucatas quebradas',
+    ['lj_deliver_button']         = 'pressione ~INPUT_PICKUP~ para entregar as partes quebradas para o guarda.'
+}
